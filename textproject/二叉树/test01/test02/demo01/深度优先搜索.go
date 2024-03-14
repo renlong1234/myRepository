149a7f6c539f7f24f66b6724a4268de48d3b65a9
renlong1234 <75119621+renlong1234@users.noreply.github.com>
2024-03-14 18:20:24 +0800

    Add files via upload

---
+++ textproject/二叉树/test01/test02/demo01/深度优先搜索.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	fmt.Println(maxDepth(root))
+}
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
