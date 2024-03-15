d76e23aec87f5e7a28c1a4f6f9ef0139bc3d1a27
renlong1234 <75119621+renlong1234@users.noreply.github.com>
2024-03-15 20:30:14 +0800

    Update 广度优先搜索.go

--- textproject/二叉树/test02/demo02/广度优先搜索.go
+++ textproject/二叉树/test02/demo02/广度优先搜索.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import "fmt"
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	//      3
+	//    /   \
+	//   9     20
+	//         / \
+	//        15   7
+	root := &TreeNode{Val: 3}
+	root.Left = &TreeNode{Val: 9}
+	root.Right = &TreeNode{Val: 20}
+	root.Right.Left = &TreeNode{Val: 15}
+	root.Right.Right = &TreeNode{Val: 7}
+
+	fmt.Println(maxDepth(root))
+}
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{}
+	queue = append(queue, root)
+	n := 0
+	for len(queue) > 0 {
+		sz := len(queue)
+		for sz > 0 {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			sz--
+		}
+		n++
+	}
+	return n
 }
