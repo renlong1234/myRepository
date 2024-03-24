cba2fafddcd1e26225c1a96d68512cfdcaa0123a
renlong1234 <75119621+renlong1234@users.noreply.github.com>
2024-03-24 21:17:03 +0800

    Update 双指针.go

--- textproject/链表/test04/demo02/双指针.go
+++ textproject/链表/test04/demo02/双指针.go
@@ -25,12 +25,19 @@ func hasCycle(head *ListNode) bool {
 
 func main() {
 	head := &ListNode{Val: 3}
-	head.Next = &ListNode{Val: 2}
-	head.Next.Next = &ListNode{Val: 0}
-	head.Next.Next.Next = &ListNode{Val: -4}
+	node02 := &ListNode{Val: 2}
+	node03 := &ListNode{Val: 0}
+	node04 := &ListNode{Val: -4}
+	head.Next = node02
+	node02.Next = node03
+	node03.Next = node04
 
-	head.Next.Next.Next.Next = head.Next
-
+	node04.Next = node02
 	h := hasCycle(head)
+	fmt.Println(h)
+	//取消环形链表
+	node04.Next = nil
+
+	h = hasCycle(head)
 	fmt.Println(h)
 }
